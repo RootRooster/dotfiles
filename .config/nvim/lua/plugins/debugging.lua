2ae10810b31835a30742278bfacd341b6259024a
Nik Cadez <nikcadez@Niks-MacBook-Pro-2.local>
2024-06-07 07:15:27 +0200

    deleted absolete file and updated keybindings

--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -3,11 +3,11 @@ return {
         "mfussenegger/nvim-dap",
         config = function()
             local dap = require("dap")
-            vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
-            vim.keymap.set("n", "<F9>", dap.step_over, {})
-            vim.keymap.set("n", "<F8>", dap.step_into, {})
-            vim.keymap.set("n", "<F7>", dap.step_out, {})
-            vim.keymap.set("n", "<F5>", dap.continue, {})
+            vim.keymap.set("n", "<leader>tb", dap.toggle_breakpoint, {})
+            vim.keymap.set("n", "<leader>s", dap.step_over, {})
+            vim.keymap.set("n", "<leader>si", dap.step_into, {})
+            vim.keymap.set("n", "<leader>so", dap.step_out, {})
+            vim.keymap.set("n", "<leader>db", dap.continue, {})
         end,
     },
     {
