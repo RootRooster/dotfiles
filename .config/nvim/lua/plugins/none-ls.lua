8c1073b4ab2547b661e2c35158b3471549edaada
root_rooster <gmail@nikcadez.com>
2024-06-07 20:56:26 +0200

    added null extras

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,21 +1,29 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				-- when adding new sources install them with mason
-				-- lua
-				null_ls.builtins.formatting.stylua,
-				-- python
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.diagnostics.pylint,
-				-- javascript
-				null_ls.builtins.formatting.prettier,
-			},
-		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {}) -- this is for formatting
-	end,
+            null_ls.setup({
+                sources = {
+                    -- when adding new sources install them with mason
+                    -- lua
+                    null_ls.builtins.formatting.stylua,
+                    -- python
+                    null_ls.builtins.formatting.isort,
+                    null_ls.builtins.formatting.black,
+                    null_ls.builtins.diagnostics.pylint,
+                    -- javascript
+                    null_ls.builtins.formatting.prettie,
+                },
+            })
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {}) -- this is for formatting
+        end,
+    },
+    {
+        "nvimtools/none-ls.nvim",
+        dependencies = {
+            "nvimtools/none-ls-extras.nvim",
+        },
+    },
 }
