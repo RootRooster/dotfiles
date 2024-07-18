7f7d94f4a32a7ab3eaf59d3ffe28a5d7f6434834
root_rooster <gmail@nikcadez.com>
2024-07-18 23:32:48 +0200

    emmet and auto autopairs

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,40 +1,44 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "pylsp",
-                    "tsserver",
-                },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"pylsp",
+					"tsserver",
+					"emmet_language_server",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities() -- for completion
+			lspconfig.lua_ls.setup({
+				capabilities, -- this will enable autocompletion for the lua lsp
+			})
+			lspconfig.pylsp.setup({
+				capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities,
+			})
+            lspconfig.emmet_language_server.setup({
+                capabilities
             })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = require("cmp_nvim_lsp").default_capabilities() -- for completion
-            lspconfig.lua_ls.setup({
-                capabilities,                                          -- this will enable autocompletion for the lua lsp
-            })
-            lspconfig.pylsp.setup({
-                capabilities,
-            })
-            lspconfig.tsserver.setup({
-                capabilities,
-            })
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {}) -- Code Actions are operations that include suggestions for refactoring, fixing errors, or implementing methods.
-        end,
-    },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {}) -- Code Actions are operations that include suggestions for refactoring, fixing errors, or implementing methods.
+		end,
+	},
 }
---
+++ .config/nvim/lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,8 @@
+return {
+    "windwp/nvim-autopairs",
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+    event = "InsertEnter",
+    config = true,
+        
+}
